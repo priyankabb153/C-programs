e86b4af514e6b1dfd97b7d035db1e349896d9c28
Priyanka Bhadrappanavar <v.bhadrappanavar@f5.com>
2021-03-23 23:55:18 +0530

    added new condition

--- voting.c
+++ voting.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int main(){
    int age=10;
-   if(age>=18)
+   if(age>18)
    {
        printf("eligible to vote\n");
    }
-   else{
+   else if(age==18){
+       printf("will be eligible from this year\n");
+       
+    }
+    else{
        printf("not eligible to vote\n");
    }
 
